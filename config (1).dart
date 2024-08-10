07fb47b02c09b53984d54200309de8b6a01ea4e2
Syed Itrat Hussain Zaidi <120056137+itratzaidi@users.noreply.github.com>
2024-08-09 20:29:22 -0700

    Add files via upload

---
+++ config (1).dart
@@ -0,0 +1,4 @@
+String gplacesAPIKey = "AIzaSyB8AIKYmPG-s5ZtMPIAisWQ6dvGDLdt1iA";
+const supabaseUrl = 'https://eopekpbiptqdalfapttf.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvcGVrcGJpcHRxZGFsZmFwdHRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQyOTMxMTIsImV4cCI6MjAyOTg2OTExMn0.KMJm9vmIDwB7G-dz33G2LWqr-tGKWiQhrROmRG8tna0';
